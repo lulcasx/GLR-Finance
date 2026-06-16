38fce4d84fda13d66c0456dcf9f28761187714f4
Lucas Alvarenga <lafernandes@sga.pucminas.br>
2026-06-16 11:10:19 -0300

    Movendo frontend para raiz

--- js/script.js
+++ js/script.js
@@ -139,3 +139,10 @@ function atualizarTela() {
             }
         );
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(() => {
+            console.log('Service Worker registrado');
+        });
+}
